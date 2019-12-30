362298e04e30c1750be92745025df8dea8c86275
RNubla <loopyelbarto@gmail.com>
2019-12-30 00:14:35 -0500

    Play run animation when right arrow key is pressed

--- main.go
+++ main.go
@@ -9,74 +9,72 @@ const (
 	minFrameSpeed = 1
 )
 
-func main() {
-	screenWidth := int32(800)
-	screenHeight := int32(450)
+var adventurerSheet rl.Texture2D
+var screenWidth int32
+var screenHeight int32
 
-	rl.InitWindow(screenWidth, screenHeight, "raylib [textures] example - texture loading and drawing")
+var currentFrame float32
+var frameCounter int
+var frameSpeed int
+
+var sourceRect rl.Rectangle
+var runRect rl.Rectangle
+var destRec rl.Rectangle
+var origin rl.Vector2
+
+func main() {
+	screenWidth = int32(800)
+	screenHeight = int32(450)
+
+	rl.InitWindow(screenWidth, screenHeight, "Sprite Sheet Animation")
 
 	// NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
-	adventurerSheet := rl.LoadTexture("assets/player/adventurer-Sheet.png") // Texture loading width:50px height:37px each frame
+	adventurerSheet = rl.LoadTexture("assets/player/adventurer-Sheet.png") // Texture loading width:50px height:37px each frame
 
-	currentFrame := float32(0)
+	// currentFrame = float32(0)
 
-	framesCounter := 0
-	framesSpeed := 8 // Number of spritesheet frames shown by second
+	// frameCounter = 0
+	frameSpeed = 8 // Number of spritesheet frames shown by second
 
 	frameWidth := float32(adventurerSheet.Width) / 7
 	frameHeight := float32(adventurerSheet.Height) / 11
 
-	sourceRect := rl.NewRectangle(0, 0, frameWidth, frameHeight)
-	// sourceRect_run := rl.NewRectangle(50,)
+	sourceRect = rl.NewRectangle(0, 0, frameWidth, frameHeight)
+	runRect := rl.NewRectangle(50, 37, frameWidth, frameHeight)
 
 	//centers sprite on screen and inrease its size
-	destRec := rl.NewRectangle(float32(screenWidth)/2, float32(screenHeight)/2, frameWidth*3, frameHeight*3)
-	origin := rl.NewVector2(frameWidth, frameHeight)
+	destRec = rl.NewRectangle(float32(screenWidth)/2, float32(screenHeight)/2, frameWidth*3, frameHeight*3)
+	origin = rl.NewVector2(frameWidth, frameHeight)
 
 	rl.SetTargetFPS(60)
 
 	for !rl.WindowShouldClose() {
-		framesCounter++
-
-		if framesCounter >= (60 / framesSpeed) {
-			framesCounter = 0
-			currentFrame++
-
-			//idle
-			// if currentFrame > 3 {
-			// 	currentFrame = 0
-			// }
-
-			// sourceRect.X = currentFrame * float32(adventurerSheet.Width) / 7
-
-			if rl.IsKeyDown(rl.KeyRight) {
-				sourceRectRun := rl.NewRectangle(50, 37, frameWidth, frameHeight)
-				// sourceRect := rl.NewRectangle(50, frameHeight, frameWidth, frameHeight)
-
+		frameCounter++
+		if rl.IsKeyDown(rl.KeyRight) {
+			if frameCounter >= (60 / frameSpeed) {
+				frameCounter = 0
+				currentFrame++
 				if currentFrame > 6 {
 					currentFrame = 1
 				}
-
-				sourceRectRun.X = currentFrame * float32(adventurerSheet.Width) / 7
-				println("KeyRight is pressed down")
-			} else if currentFrame > 3 {
-				currentFrame = 0
 			}
-			sourceRect.X = currentFrame * float32(adventurerSheet.Width) / 7
+			runRect.X = currentFrame * float32(adventurerSheet.Width) / 7
+			rl.DrawRectangleLines(15+int32(runRect.X), 40+int32(runRect.Y), int32(runRect.Width), int32(runRect.Height), rl.Red)
+			rl.DrawTexturePro(adventurerSheet, runRect, destRec, origin, 0, rl.White)
 		}
-		// println("Current Frame: ", int(currentFrame))
+		if rl.IsKeyUp(rl.KeyRight) {
+			if frameCounter >= (60 / frameSpeed) {
+				frameCounter = 0
+				currentFrame++
+				if currentFrame > 3 {
+					currentFrame = 0
+				}
+				sourceRect.X = currentFrame * float32(adventurerSheet.Width) / 7
+			}
+			rl.DrawRectangleLines(15+int32(sourceRect.X), 40+int32(sourceRect.Y), int32(sourceRect.Width), int32(sourceRect.Height), rl.Red)
 
-		// if rl.IsKeyDown(rl.KeyRight) {
-		// 	framesSpeed++
-		// } else if rl.IsKeyDown(rl.KeyLeft) {
-		// 	framesSpeed--
-		// }
-
-		// if framesSpeed > maxFrameSpeed {
-		// 	framesSpeed = maxFrameSpeed
-		// } else if framesSpeed < minFrameSpeed {
-		// 	framesSpeed = minFrameSpeed
-		// }
+			rl.DrawTexturePro(adventurerSheet, sourceRect, destRec, origin, 0, rl.White)
+		}
 
 		rl.BeginDrawing()
 
@@ -84,9 +82,6 @@ func main() {
 
 		rl.DrawTexture(adventurerSheet, 15, 40, rl.White)
 		rl.DrawRectangleLines(15, 40, adventurerSheet.Width, adventurerSheet.Height, rl.Lime)
-		rl.DrawRectangleLines(15+int32(sourceRect.X), 40+int32(sourceRect.Y), int32(sourceRect.Width), int32(sourceRect.Height), rl.Red)
-
-		rl.DrawTexturePro(adventurerSheet, sourceRect, destRec, origin, 0, rl.White)
 
 		rl.DrawText("(c) texture sprite by Eiden Marsal", screenWidth-200, screenHeight-20, 10, rl.Gray)
 
@@ -96,4 +91,17 @@ func main() {
 	rl.UnloadTexture(adventurerSheet)
 
 	rl.CloseWindow()
+}
+func idleAnim(frameCnt int, currFrame float32, srcRect rl.Rectangle, advenSheet rl.Texture2D) float32 {
+	frameCnt = 0
+	currFrame++
+	if currFrame > 3 {
+		currFrame = 0
+	}
+	a := &currFrame
+	srcRect.X = currFrame * float32(advenSheet.Width) / 7
+	println("CurrFrameAdd", a)
+	println("CurrentFrame: ", int(*a))
+
+	return currFrame
 }
